87a95f8a6aa97c5418e3e2d7229cffe43304753b
Zakaria Muhumed <zakariamuhumed25@gmial.com>
2021-10-11 18:23:26 -0500

    express sessions part2

--- app.js
+++ app.js
@@ -45,50 +45,37 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
-  if(!req.session.user) {
-      const authHeader = req.headers.authorization;
-      if (!authHeader) {
-          const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          return next(err);
-      }
 
-      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      const user = auth[0];
-      const pass = auth[1];
-      if (user === 'admin' && pass === 'password') {
-           req.session.user = 'admin';
-          return next(); // authorized
-      } else {
-          const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');      
-          err.status = 401;
-          return next(err);
-      }
- }else{
-   if(req.session.user === 'admin') {
-     return next();
-   }else{
-      const err = new Error('You are not authenticated!');    
+  if (!req.session.user) {
+      const err = new Error('You are not authenticated!');
       err.status = 401;
       return next(err);
-   }
- }      
+  } else {
+      if (req.session.user === 'authenticated') {
+          return next();
+      } else {
+          const err = new Error('You are not authenticated!');
+          err.status = 401;
+          return next(err);
+      }
+  }
 }
 
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/campsite', campsiteRouter);
 app.use('/promotion', promotionRouter);
 app.use('/promotion/:promotionId', promotionRouter);
 app.use('/partner', partnerRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
