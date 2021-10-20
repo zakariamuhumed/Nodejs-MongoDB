6890c9834d228a95944b753063fbe2787b4ec742
Zakaria Muhumed <zakariamuhumed25@gmial.com>
2021-10-19 19:09:51 -0500

    cross origing resource sharing

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const authenticate = require('../authenticate');
 const multer = require('multer');
+const cors = require('./cors');
+
 
 
 const storage = multer.diskStorage({
@@ -24,22 +26,23 @@ const upload = multer({storage: storage, fileFilter: imageFileFilter});
 const uploadRouter = express.Router();
 
 uploadRouter.route('/')
-.get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.cors, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     res.status = 403;
     res.end('GET  operation not supported on /imageUpload');
 })
 uploadRouter.route('/')
-.post(authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req, res) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req, res) => {
     res.status = 200;
     res.json(req.file);
 })
 uploadRouter.route('/')
-.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     res.status = 403;
     res.end('GET  operation not supported on /imageUpload');
 })
 uploadRouter.route('/')
-.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     res.status = 403;
     res.end('GET  operation not supported on /imageUpload');
 })
