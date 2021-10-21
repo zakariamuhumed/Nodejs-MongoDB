376221b9fa199a4aa47956a1ba13920ca8afa17a
Zakaria Muhumed <zakariamuhumed25@gmial.com>
2021-10-20 19:02:01 -0500

    using oauth with passport and facebook

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '614710682889095',
+        clientSecret: '3ab54b2e967bbe28423d01f6b3081ee6'
+    }
+
 }
