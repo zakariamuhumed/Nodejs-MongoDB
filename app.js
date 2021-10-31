ac945d647d7849278a9d23a12cc67895a68df645
Zakaria Muhumed <zakariamuhumed25@gmial.com>
2021-10-31 17:25:33 -0500

    final assingment

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favorite');
 
 const mongoose = require('mongoose');
 
@@ -59,7 +60,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/favorites', favoriteRouter);
 app.use('/imageUpload', uploadRouter);
+
 
 
 // catch 404 and forward to error handler
